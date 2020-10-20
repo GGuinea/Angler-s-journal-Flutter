19131e0c8a6d9885c87ba9254481772cd69c0c70
Kamil_Zielinski <215933@edu.p.lodz.pl>
2020-10-20 20:29:17 +0200

    Added adding new entries to the dairy - working fine, lack of validation. Without database.

--- lib/fishingDiary.dart
+++ lib/fishingDiary.dart
@@ -8,6 +8,13 @@ class Diary extends StatefulWidget {
 }
 
 class _DiaryState extends State<Diary> {
+  final dateControllerStart = TextEditingController();
+  final dateControllerEnd = TextEditingController();
+  String title = "";
+  String waterName = "";
+  String description = "";
+  String dateStart = "";
+  String dateEnd = "s";
   List<FishingEntry> fishingEntries = [
     FishingEntry("Polowy", "assets/images/fish.png", "Nawet spoko",
         "Zalew slok", "data"),
@@ -17,8 +24,11 @@ class _DiaryState extends State<Diary> {
         "Polowy samemu", "assets/images/fish.png", "slabo", "ZPT", "data3"),
   ];
 
-  void onTabTapped() {
-    print("Debug message");
+  @override
+  void dispose() {
+    dateControllerStart.dispose();
+    dateControllerEnd.dispose();
+    super.dispose();
   }
 
   @override
@@ -35,7 +45,7 @@ class _DiaryState extends State<Diary> {
                   itemCount: fishingEntries.length,
                   itemBuilder: (context, index) => ListTile(
                     title: FishingCard(fishingEntry: fishingEntries[index]),
-                    onTap: onTabTapped,
+                    onTap: () {},
                   ),
                 ),
               )
@@ -45,11 +55,121 @@ class _DiaryState extends State<Diary> {
         floatingActionButton: FloatingActionButton(
             backgroundColor: Colors.grey,
             splashColor: Colors.blue,
-            onPressed: () {},
+            onPressed: showAlertDialog,
             child: Icon(
               Icons.add,
               color: Colors.blue,
               size: 30,
             )));
+  }
+
+  void showAlertDialog() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return StatefulBuilder(builder: (context, setState) {
+            return AlertDialog(
+              insetPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              content: SingleChildScrollView(
+                child: Container(
+                  width: MediaQuery.of(context).size.width - 20,
+                  child: Column(
+                    children: [
+                      Center(
+                        child: Text("Wprowadz informacje"),
+                      ),
+                      TextField(
+                          decoration: InputDecoration(
+                            labelText: 'Tytul wyprawy',
+                          ),
+                          onChanged: (val) {
+                            title = val;
+                          }),
+                      TextField(
+                          decoration: InputDecoration(
+                            labelText: 'Nazwa zbiornika',
+                          ),
+                          onChanged: (val) {
+                            waterName = val;
+                          }),
+                      TextField(
+                          decoration: InputDecoration(
+                            labelText: 'Dodatkowe informacje',
+                          ),
+                          onChanged: (val) {
+                            print(val);
+                            description = val;
+                          }),
+                      TextField(
+                          readOnly: true,
+                          controller: dateControllerStart,
+                          decoration: InputDecoration(
+                            hintText: "Poczatek",
+                          ),
+                          onTap: () async {
+                            var date = await showDatePicker(
+                              context: context,
+                              initialDate: DateTime.now(),
+                              firstDate: DateTime(2000),
+                              lastDate: DateTime(2100),
+                            );
+                            dateControllerStart.text =
+                                date.toString().substring(0, 10);
+                            dateStart = dateControllerStart.text;
+                          }),
+                      TextField(
+                          readOnly: true,
+                          controller: dateControllerEnd,
+                          decoration: InputDecoration(
+                            hintText: "Koniec",
+                          ),
+                          onTap: () async {
+                            var date = await showDatePicker(
+                              context: context,
+                              initialDate: DateTime.now(),
+                              firstDate: DateTime(2000),
+                              lastDate: DateTime(2100),
+                            );
+                            dateControllerEnd.text =
+                                date.toString().substring(0, 10);
+                            dateEnd = dateControllerEnd.text;
+                          }),
+                      Padding(
+                        padding: EdgeInsets.only(top: 4),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          RaisedButton(
+                            onPressed: addFishEntry,
+                            color: Colors.blueAccent,
+                            child: Text(
+                              "Dodaj",
+                            ),
+                          ),
+                          RaisedButton(
+                            onPressed: () => Navigator.pop(context, false),
+                            color: Colors.blueAccent,
+                            child: Text(
+                              "Anuluj",
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            );
+          });
+        });
+  }
+
+  void addFishEntry() {
+    Navigator.pop(context);
+    setState(() {
+      fishingEntries.add(FishingEntry(
+          title, 'assets/images/fish.png', description, waterName, dateStart));
+    });
   }
 }
