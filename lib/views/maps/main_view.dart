724c04c590cbedbe8d0a2ea80131873c0716522a
Kamil_Zielinski <215933@edu.p.lodz.pl>
2020-11-30 19:05:05 +0100

    Working location

--- lib/views/maps/main_view.dart
+++ lib/views/maps/main_view.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location_permissions/location_permissions.dart';
 
@@ -10,32 +11,99 @@ class FishingMap extends StatefulWidget {
 }
 
 class _FishingMapState extends State<FishingMap> {
-  Completer<GoogleMapController> _controller = Completer();
+  static const LatLng _center = const LatLng(50.50, 18.18);
 
-  static const LatLng _center = const LatLng(0, 0);
+  GoogleMapController mapController;
   PermissionStatus permisison;
+  final Set<Marker> _markers = {};
+  LatLng _lastMapPosition = _center;
+  Geolocator _geolocator = Geolocator();
+  Position _currentPosition;
 
-  void _getPermission() async {
-    permisison = await LocationPermissions().requestPermissions();
-  }
-
-  void _onMapCreated(GoogleMapController controller) {
-    _controller.complete(controller);
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
   }
 
   @override
   Widget build(BuildContext context) {
-    _getPermission();
     return Scaffold(
       appBar: AppBar(
         title: Text('Mapy'),
       ),
-      body: GoogleMap(
-        onMapCreated: _onMapCreated,
-        initialCameraPosition: CameraPosition(
-          target: _center,
-        ),
+      body: Stack(
+        children: [
+          GoogleMap(
+            onMapCreated: _onMapCreated,
+            zoomGesturesEnabled: true,
+            myLocationEnabled: true,
+            initialCameraPosition: CameraPosition(
+              target: _center,
+              zoom: 10.0,
+            ),
+            markers: _markers,
+            onCameraMove: _onCameraMove,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15),
+            child: Align(
+              alignment: Alignment.topRight,
+              child: Column(children: [
+                FloatingActionButton(
+                  onPressed: _onAddMarkerButtonPressed,
+                  materialTapTargetSize: MaterialTapTargetSize.padded,
+                  backgroundColor: Colors.orange,
+                  child: const Icon(Icons.add_location, size: 30),
+                )
+              ]),
+            ),
+          )
+        ],
       ),
     );
+  }
+
+  _getCurrentLocation() async {
+    await _geolocator
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
+        .then((Position position) async {
+      setState(() {
+        _currentPosition = position;
+        mapController.animateCamera(
+          CameraUpdate.newCameraPosition(
+            CameraPosition(
+              target: LatLng(position.latitude, position.longitude),
+              zoom: 18.0,
+            ),
+          ),
+        );
+      });
+    }).catchError((e) {
+      print(e);
+    });
+  }
+
+  void _onAddMarkerButtonPressed() {
+    setState(() {
+      _markers.add(Marker(
+        // This marker id can be anything that uniquely identifies each marker.
+        markerId: MarkerId(_lastMapPosition.toString()),
+        position: _lastMapPosition,
+        infoWindow: InfoWindow(
+          title: 'Really cool place',
+          snippet: '5 Star Rating',
+        ),
+        icon: BitmapDescriptor.defaultMarker,
+      ));
+    });
+  }
+
+  void _onCameraMove(CameraPosition position) {
+    _lastMapPosition = position.target;
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
   }
 }
