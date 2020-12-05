0366afd810823bb1201b0f0566ca07b6aa5bf15e
Kamil_Zielinski <215933@edu.p.lodz.pl>
2020-12-05 19:55:12 +0100

    Fixed test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,4 +1,3 @@
-
 // This is a basic Flutter widget test.
 //
 // To perform an interaction with a widget in your test, use the WidgetTester
@@ -8,7 +7,7 @@
 
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:NotatnikWedkarza/main.dart';
+import 'package:notatinik_wedkarza/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
